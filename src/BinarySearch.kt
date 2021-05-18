758db997ad0774b965bd9df4faf1d3ffe568a813
Manthan <m.a1449pithadia@gmail.com>
2021-05-18 10:53:05 +0530

    test Commit Final

--- src/BinarySearch.kt
+++ src/BinarySearch.kt
@@ -1,16 +1,21 @@
 fun main() {
-  var array = arrayOf(5,6,15,20,60,80)
-  var binarySearch = BinarySearch(array)
-
+  var array = intArrayOf(5, 6, 15, 20, 60, 80)
+  val index = binarySearch(array,60,0,array.size-1)
+  when{
+    index > 0 -> System.out.println("Element found at index $index")
+    else -> System.out.println("Element not found")
+  }
 }
 
+fun binarySearch(array: IntArray,num:Int, start:Int, end:Int): Int{
 
-class BinarySearch constructor(array: Array<Int>){
-
-  companion object{
-    fun searchNumber(num:Int){
-
+  while (start<=end){
+    val mid = (start+end)/2
+    when{
+      num > array[mid] -> return binarySearch(array,num,mid+1,end)
+      num < array[mid] -> return binarySearch(array,num,start,mid-1)
+      num == array[mid] -> return mid
     }
   }
-
+  return -1
 }
