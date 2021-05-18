993a0a1cc5dfd135ca91f2925ce09d01d285a0a7
Developer1449 <m.a1449pithadia@gmail.com>
2021-05-18 11:57:06 +0530

    s

--- src/BinarySearch.kt
+++ src/BinarySearch.kt
@@ -1,5 +1,5 @@
 fun main() {
-    System.out.println("Hello world With Changes with SSH")
+    System.out.println("Hello world With Changes with")
 }
 
 fun binarySearch(array: IntArray,num:Int, start:Int, end:Int): Int{
